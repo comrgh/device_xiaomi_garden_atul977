be41039d88225013e083469de1861ec3cfa39390
adarsh956 <adarshsingh8312@gmail.com>
2023-01-31 23:28:01 +0530

    garden: vendorsetup: Track our own FaceUnlock Repos
    Signed-off-by: adarsh956 <adarshsingh8312@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,12 @@ rm -rf frameworks/opt/net/ims
 git clone https://github.com/ArrowOS/android_frameworks_opt_telephony.git -b arrow-13.0 frameworks/opt/telephony
 git clone https://github.com/ArrowOS/android_frameworks_opt_net_ims.git -b arrow-13.0 frameworks/opt/net/ims
 
+# Face Unlock Repos
+rm -rf external/faceunlock
+rm -rf packages/apps/FaceUnlockService
+git clone https://gitlab.com/adarsh956/external_faceunlock.git external/faceunlock
+git clone https://gitlab.com/adarsh956/packages_apps_FaceUnlockService.git packages/apps/FaceUnlockService
+
 # Apply Patches
 git clone https://github.com/Adarsh956/patches
 bash patches/apply-patches.sh
